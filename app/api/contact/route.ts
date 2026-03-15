9763c26835b5750c2a46a999106f84ebcbf8ff6e
unknown <capitanmeseros2017@gmail.com>
2026-03-14 20:59:25 -0500

    configuracion de nodemailer

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,30 +1,35 @@
 import { NextResponse } from "next/server"
-import nodemailer from "nodemailer" 
+import nodemailer from "nodemailer"
 
 export async function POST(req: Request) {
 
-  const data = await req.formData()
+  try {
 
-  const nombre = data.get("nombre")
-  const telefono = data.get("telefono")
-  const email = data.get("email")
-  const mensaje = data.get("mensaje")
+    const data = await req.formData()
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.hostinger.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "jyleventos.com.co",
-      pass: "TU_PASSWORD"
-    }
-  })
+    const nombre = data.get("nombre")
+    const telefono = data.get("telefono")
+    const email = data.get("email")
+    const mensaje = data.get("mensaje")
 
-  await transporter.sendMail({
-    from: `"Formulario Web" <contacto@jyleventos.com.co>`,
-    to: "contacto@jyleventos.com.co",
-    subject: "Nueva cotización desde la web",
-    text: `
+    const transporter = nodemailer.createTransport({
+  host: "smtp.hostinger.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+})
+
+    await transporter.sendMail({
+      from: `"Formulario Web" <contacto@jyleventos.com.co>`,
+      to: "contacto@jyleventos.com.co",
+      subject: "Nueva cotización desde la web",
+      text: `
 Nombre: ${nombre}
 Teléfono: ${telefono}
 Email: ${email}
@@ -32,8 +37,19 @@ Email: ${email}
 Mensaje:
 ${mensaje}
 `
-  })
+    })
 
-  return NextResponse.json({ success: true })
+    return NextResponse.json({ success: true })
+
+  } catch (error) {
+
+    console.error(error)
+
+    return NextResponse.json(
+      { success: false },
+      { status: 500 }
+    )
+
+  }
 
 }
